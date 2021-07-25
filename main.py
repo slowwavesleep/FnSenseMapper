6c825f6d75182fe534e4a51daba691913ab7e7a5
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2021-07-25 23:42:47 +0300

    Add yaml config

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
+import argparse
+
+from yaml import safe_load
+
 from ranker import CandidateRanker
 from relation_generator import RelationGenerator
 
 if __name__ == "__main__":
 
-    DO_RANKING = True
-    DATA_PATH = "resources/bn_entries.csv"
-    NO_HEADER = True
-    SUBSTRING_CONDITION = True
-    N_CANDIDATES = 3
-    RANKER_OUT_PATH = "resources/candidates.jsonl"
-    RELATIONS_OUT_PATH = "resources/qualia_relations.csv"
-    EDGES_PATH = "resources/bn_edges.csv"
-    MODEL_NAME = "sentence-transformers/LaBSE"
-    CUTOFF_SIMILARITY = 0.3
+    parser = argparse.ArgumentParser(description='Run FN sense mapping with specified settings')
+    parser.add_argument(dest="config", nargs="?", type=str, help="Path to config file", default="config.yml")
+    args = parser.parse_args()
 
-    if DO_RANKING:
-        ranker = CandidateRanker(data_path=DATA_PATH,
-                                 out_path=RANKER_OUT_PATH,
-                                 no_header=NO_HEADER,
-                                 model_name=MODEL_NAME,
-                                 n_candidates=N_CANDIDATES,
-                                 substring_condition=SUBSTRING_CONDITION,
-                                 cutoff_similarity=CUTOFF_SIMILARITY)
+    with open(args.config) as file:
+        config = safe_load(file)
+
+    data_path = config["paths"]["data_path"]
+    edges_path = config["paths"]["edges_path"]
+    ranker_out_path = config["paths"]["ranker_out_path"]
+    relations_out_path = config["paths"]["relations_out_path"]
+
+    do_ranking = config.get("do_ranking", True)
+    no_header = config.get("no_header", True)
+    substring_condition = config.get("substring_condition", True)
+
+    embedder_model = config["embedder_model"]
+
+    cutoff_similarity = config["cutoff_similarity"]
+    n_candidates = config.get("n_candidates", 1)
+
+    if do_ranking:
+        ranker = CandidateRanker(data_path=data_path,
+                                 out_path=ranker_out_path,
+                                 no_header=no_header,
+                                 model_name=embedder_model,
+                                 n_candidates=n_candidates,
+                                 substring_condition=substring_condition,
+                                 cutoff_similarity=cutoff_similarity)
 
         ranker.write_candidates()
 
-    relations_gen = RelationGenerator(edges_path=EDGES_PATH,
-                                      candidates_path=RANKER_OUT_PATH,
-                                      entries_path=DATA_PATH,
-                                      no_header=NO_HEADER,
-                                      out_path=RELATIONS_OUT_PATH)
+    relations_gen = RelationGenerator(edges_path=edges_path,
+                                      candidates_path=ranker_out_path,
+                                      entries_path=data_path,
+                                      no_header=no_header,
+                                      out_path=relations_out_path)
 
     relations_gen.process_data()
 
